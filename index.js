53526ed73ab734df02d1fcbe72709d0183c498ef
Joaquin Flores <joaflores715@gmail.com>
2022-06-27 03:56:08 -0300

    form funcional

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 const redirect = (url) => window.open(url, '_blank');
+const $form = document.querySelector('form');
+const $mailto =document.querySelector('#mailto') 
+
+$form.addEventListener('submit',handleSubmit)
+function handleSubmit(event){
+    event.preventDefault();
+    const form = new FormData(this)
+    console.log(form.get('name'))
+    $mailto.setAttribute('href',`mailto:joaflores899@gmail.com?subject=
+    ${form.get('name')}
+    ${form.get('email')}
+    &body=${form.get('message')}
+    
+    `)
+    $mailto.click()
+    
+}
